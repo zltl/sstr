92cd3ed79b2707de82594dede784cb9ed9914db1
liaotonglang <liaotonglang@gmail.com>
2022-03-01 20:20:29 +0800

    more documents of sstr.h

--- sstr.h
+++ sstr.h
@@ -14,7 +14,36 @@ extern "C" {
 #endif
 
 /**
- * @brief string type
+ * @brief sstr_t are objects that represent sequences of characters.
+ * @details The standard C style string is a sequence of characters terminated
+ * by a null character, which is easy to cause buffer overflow. And it's
+ * annoying to pass pointer and length of string to every whare.
+ *
+ * The sequence of characters just like a string in C++, inside the sstr_t
+ * struct, it also has a null character at the end, so that we can use
+ * sstr_t as a C style string.
+ *
+ * sstr_t contains a pointer to char sequence and its length, solves the
+ * security problems of standard C string. With functions bundle with sstr_t,
+ * you can easily manipulate the string just like standard C string, but in a
+ * safer way:
+ *
+ *     sstr_t stotal = sstr_new();
+ *     sstr_t s1 = sstr("hello");
+ *     sstr_t s2 = sstr("world");
+ *     sstr_append(stotal, s1);
+ *     sstr_append_of(stotal, " ", 1);
+ *     sstr_append(stotal, s2);
+ *     sstr_free(s1);
+ *     sstr_free(s2);
+ *
+ *     sstr_t result = sstr_printf("stotal=%S, c-str=%s, int=%d, long=%ld",
+ * stotal, stotal, 123, (long)456);
+ *
+ *     print(sstr_cstr(result));
+ *
+ *     sstr_free(result);
+ *     sstr_free(stotal);
  */
 typedef void* sstr_t;
 
@@ -34,6 +63,8 @@ void sstr_free(sstr_t s);
 
 /**
  * @brief Create a sstr_t from \a data with \a length bytes.
+ * @details The \a data is copied to the new sstr_t, so you can free \a data
+ * after calling this function.
  *
  * @param data data to copy to the result sstr_t.
  * @param length length of \a data.
@@ -43,6 +74,8 @@ sstr_t sstr_of(const void* data, size_t length);
 
 /**
  * @brief Create a sstr_t from C-style (NULL-terminated) string \a str.
+ * @details The \a cstr is copied to the new sstr_t, so you can free \a cstr
+ * after calling this function.
  *
  * @param cstr C-style string to copy to the result sstr_t.
  * @return sstr_t containing \a data copied from cstr.
@@ -51,18 +84,30 @@ sstr_t sstr(const char* cstr);
 
 /**
  * @brief Return C-style string representation of \a s.
+ * @details This function return a pointer to the internal C-style string, it
+ * has a null-terminal character at the end. So you can use it as a C-style
+ * string. The returned pointer is valid until
+ * sstr_free()/sstr_append()/sstr_append_of() or any functions that may modify
+ * the contents of sstr_t is called.
  *
  * @param s sstr_t instance to convert to C-style string.
  * @return char* C-style string representation of \a s.
- * @note The returned string is reused by \a s, do not free it.
+ * @note The returned string is reused by \a s, do not free it yourself.
  */
 char* sstr_cstr(sstr_t s);
 
 /**
- * @brief Return length of \a s.
+ * @brief Return the length of \a s, in terms of bytes.
+ * @details This is the number of actual bytes that conform the contents of the
+ * sstr_t, which is not necessarily equal to its storage capacity.
+ *
+ * Note that sstr_t objects handle bytes without knowledge of the encoding that
+ * may eventually be used to encode the characters it contains. Therefore, the
+ * value returned may not correspond to the actual number of encoded characters
+ * in sequences of multi-byte or variable-length characters (such as UTF-8).
  *
  * @param s sstr_t instance to get length of.
- * @return size_t length of \a s.
+ * @return size_t The number of bytes of \a s.
  */
 size_t sstr_length(sstr_t s);
 
@@ -73,19 +118,28 @@ size_t sstr_length(sstr_t s);
  * @param a sstr_t to be compared.
  * @param b sstr_t to be compared to.
  * @return int the compare result.
+ * @returns 0 They compare equal.
+ * @returns <0 Either the value of the first character that does not match is
+ * lower in the compared string, or all compared characters match but the
+ * compared string is shorter.
+ * @returns >0 Either the value of the first character that does not match is
+ * greater in the compared string, or all compared characters match but the
+ * compared string is longer.
  * @note This function is case sensitive.
  */
 int sstr_compare(sstr_t a, sstr_t b);
 
 /**
  * @brief compare sstr_t \a a and \a c-style string b
+ * @details just like sstr_compare, but compare \a a and \a c-style string b.
  *
  * @return int
  */
 int sstr_compare_c(sstr_t a, const char* b);
 
 /**
- * @brief Append \a length byte of '\0' to \a s.
+ * @brief Extends the sstr_t by appending additional '\0' characters at the end
+ * of its current value.
  *
  * @param s destination sstr_t.
  * @param length length of '\0' to append.
@@ -93,7 +147,8 @@ int sstr_compare_c(sstr_t a, const char* b);
 void sstr_append_zero(sstr_t s, size_t length);
 
 /**
- * @brief Append \a length byte of \a data to \a s.
+ * @brief Extends the sstr_t by appending additional characters in \a data with
+ * length of \a length at the end of its current value .
  *
  * @param s destination sstr_t.
  * @param data data to append.
@@ -102,7 +157,8 @@ void sstr_append_zero(sstr_t s, size_t length);
 void sstr_append_of(sstr_t s, const void* data, size_t length);
 
 /**
- * @brief Append contents of \a src to \a dst.
+ * @brief Extends the sstr_t by appending additional characters contained in \a
+ * src.
  *
  * @param dst destination sstr_t.
  * @param src source sstr_t.
@@ -110,7 +166,7 @@ void sstr_append_of(sstr_t s, const void* data, size_t length);
 void sstr_append(sstr_t dst, sstr_t src);
 
 /**
- * @brief Duplicate \a s and return
+ * @brief Duplicate \a s and return.
  *
  * @param s sstr_t to duplicate.
  * @return sstr_t  duplicate of \a s.

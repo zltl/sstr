c082573b07973a568c141ef53438e29cc393bd83
liaotonglang <liaotonglang@gmail.com>
2022-03-30 14:41:13 +0800

    advance version to 1.0.4

--- sstr.c
+++ sstr.c
@@ -558,6 +558,6 @@ static unsigned char* sstr_sprintf_num(unsigned char* buf, unsigned char* last,
 }
 
 const char* sstr_version() {
-    static const char* const version = "1.0.3";
+    static const char* const version = "1.0.4";
     return version;
 }
